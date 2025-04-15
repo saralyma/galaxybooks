f8b8043448221029792c60358a309d8e9b09eefe
Sara Lyma <saralm910@gmail.com>
2025-04-15 14:51:54 -0300

    Adiciona backend com conta pessoal via PowerShell

---
+++ backend/src/main/java/com/galaxybooks/GalaxybooksApplication.java
@@ -0,0 +1,13 @@
+package com.galaxybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxybooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxybooksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/galaxybooks/GalaxybooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxybooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
